ad297436d774a37731e196246a005949707361b8
aitva <arod.louis@gmail.com>
2017-07-27 11:04:55 +0200

    A lot of change, add a page for media type.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/gorilla/handlers"
@@ -15,7 +17,7 @@ type server struct {
 	tmpl *template.Template
 }
 
-type displayHTTP struct {
+type headInfo struct {
 	Title   string
 	Icon    string
 	Request string
@@ -37,6 +39,7 @@ func main() {
 	mux.HandleFunc("/http-basics/", s.httpBasicsHandler("http-basics.html", "HTTP Basics", "fa-coffee"))
 	mux.HandleFunc("/http-query/", s.httpBasicsHandler("http-query.html", "HTTP Query", "fa-question"))
 	mux.HandleFunc("/http-post/", s.httpPostHandler)
+	mux.HandleFunc("/http-media/", s.httpMediaHandler)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// f, err := os.Create("access.log")
@@ -57,6 +60,15 @@ func writeError(w http.ResponseWriter, msg string) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte(msg))
+}
+
+func (s *server) execTemplate(w http.ResponseWriter, file string, data interface{}) {
+	err := s.tmpl.ExecuteTemplate(w, file, data)
+	if err != nil {
+		log.Println("fail to execute template:", err)
+		writeError(w, "Oops, something went wrong... ☹")
+		return
+	}
 }
 
 func (s *server) reloadTemplate(w *fsnotify.Watcher) {
@@ -82,21 +94,16 @@ func (s *server) indexHandler(w http.ResponseWriter, r *http.Request) {
 		s.notFoundHandler(w, r)
 		return
 	}
-	d := &displayHTTP{
+	d := &headInfo{
 		Title: "AJAX Game",
 		Icon:  "fa-gamepad",
 	}
-	err := s.tmpl.ExecuteTemplate(w, "index.html", d)
-	if err != nil {
-		log.Println("fail to execute template:", err)
-		writeError(w, "Oops, something went wrong... ☹")
-		return
-	}
+	s.execTemplate(w, "index.html", d)
 }
 
 func (s *server) httpBasicsHandler(file, title, icon string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		d := &displayHTTP{
+		d := &headInfo{
 			Title: title,
 			Icon:  icon,
 		}
@@ -107,32 +114,60 @@ func (s *server) httpBasicsHandler(file, title, icon string) http.HandlerFunc {
 			return
 		}
 		d.Request = string(tmp)
-		err = s.tmpl.ExecuteTemplate(w, file, d)
-		if err != nil {
-			log.Println("fail to execute template:", err)
-			writeError(w, "Oops, something went wrong... ☹")
-			return
-		}
+		s.execTemplate(w, file, d)
 	}
 }
 
 func (s *server) httpPostHandler(w http.ResponseWriter, r *http.Request) {
-	d := struct {
-		*displayHTTP
+	d := &struct {
+		headInfo
+		Error string
 		Color string
 		Name  string
 	}{
-		displayHTTP: &displayHTTP{
+		headInfo: headInfo{
 			Title: "HTTP Post",
 			Icon:  "fa-info",
 		},
+		Error: "Sorry, I am confuse... What is your favorite color again? Did we met before?",
 	}
+	badRequestHandler := s.badRequestHandler(d)
+
 	q := r.URL.Query()
-	d.Name = q.Get("name")
+	d.Name = strings.Title(q.Get("name"))
 	d.Color = q.Get("color")
 	if d.Name == "" || d.Color == "" {
-		d.Icon = "fa-bug"
+		badRequestHandler(w, r)
+		return
 	}
+
+	tmp, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		log.Println("fail to dump request:", err)
+		writeError(w, "Oops, something went wrong... ☹")
+		return
+	}
+	d.Request = string(tmp)
+	s.execTemplate(w, "http-post.html", d)
+}
+
+func (s *server) httpMediaHandler(w http.ResponseWriter, r *http.Request) {
+	d := &struct {
+		headInfo
+		Error    string
+		Color    string
+		Name     string
+		KindWord string
+		FunGauge string
+	}{
+		headInfo: headInfo{
+			Title: "HTTP Media",
+			Icon:  "fa-sun-o",
+		},
+		Error: "Something is not quite right... Do we know each other?",
+	}
+	badRequestHandler := s.badRequestHandler(d)
+
 	tmp, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		log.Println("fail to dump request:", err)
@@ -141,16 +176,25 @@ func (s *server) httpPostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	d.Request = string(tmp)
 
-	err = s.tmpl.ExecuteTemplate(w, "http-post.html", d)
+	err = r.ParseForm()
 	if err != nil {
-		log.Println("fail to execute template:", err)
-		writeError(w, "Oops, something went wrong... ☹")
+		badRequestHandler(w, r)
 		return
 	}
+	d.Color = r.Form.Get("color")
+	d.Name = strings.Title(r.Form.Get("name"))
+	d.KindWord = r.Form.Get("kind-word")
+	d.FunGauge = r.Form.Get("fun-gauge")
+	if d.Color == "" || d.Name == "" || d.KindWord == "" || d.FunGauge == "" {
+		badRequestHandler(w, r)
+		return
+	}
+
+	s.execTemplate(w, "http-media.html", d)
 }
 
 func (s *server) notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	d := &displayHTTP{
+	d := &headInfo{
 		Title: "404 - Not Found",
 		Icon:  "fa-eye",
 	}
@@ -159,5 +203,27 @@ func (s *server) notFoundHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println("fail to execute template:", err)
 		writeError(w, "Oops, something went wrong... ☹")
 		return
+	}
+}
+
+func (s *server) badRequestHandler(data interface{}) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		v := reflect.ValueOf(data)
+		if v.Kind() == reflect.Ptr && !v.IsNil() && v.IsValid() {
+			f := v.Elem().FieldByName("Icon")
+			if f.IsValid() {
+				f.SetString("fa-bug")
+			}
+		} else {
+			log.Println("unexpected type for data (expects &struct{})")
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		err := s.tmpl.ExecuteTemplate(w, "bad-request.html", data)
+		if err != nil {
+			log.Println("fail to execute template:", err)
+			writeError(w, "Oops, something went wrong... ☹")
+			return
+		}
 	}
 }
