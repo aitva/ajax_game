303aabfae181852ee94560dc3bf295861066a992
Aitva <arod.louis@gmail.com>
2017-08-30 01:38:46 +0200

    Update parser.

--- static/js/parser.js
+++ static/js/parser.js
@@ -4,10 +4,29 @@ const Parser = function() {
     let that = {};
     
     that.parse = function(str) {
-        let toks = str.split(" ")
-        return new Request(toks[1], {
-            method: toks[0]
-        });
+        let req = {};
+        let lines = str.split("\n");
+        
+        let toks = lines[0].split(" ")
+        if (toks.length !== 3) {
+            throw 'line 1: expects 3 tokens';
+        }
+        if (toks[2] !== "HTTP/1.1") {
+            throw 'line 1: HTTP version is missing';
+        }
+        let url = toks[1];
+        req.method = toks[0]
+
+        req.headers = {};
+        for (let i of lines.slice(1)) {
+            toks = lines[i].split(":");
+            if (toks.length !== 2) {
+                throw `line ${i+1}: expects ":"`
+            }
+            req.headers[toks[0]] = toks[1];
+        }
+
+        new Request(url, req);
     }
 
     return Object.freeze(that);
