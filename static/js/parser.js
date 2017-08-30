1de5e496854a1ac9f29a12aee60f0de3e4bedea5
Aitva <arod.louis@gmail.com>
2017-08-30 02:06:08 +0200

    Parser kind of working.

--- static/js/parser.js
+++ static/js/parser.js
@@ -2,11 +2,10 @@
 
 const Parser = function() {
     let that = {};
-    
+    let allSpace = /^\s+$/;
     that.parse = function(str) {
         let req = {};
         let lines = str.split("\n");
-        
         let toks = lines[0].split(" ")
         if (toks.length !== 3) {
             throw 'line 1: expects 3 tokens';
@@ -15,18 +14,31 @@ const Parser = function() {
             throw 'line 1: HTTP version is missing';
         }
         let url = toks[1];
-        req.method = toks[0]
-
+        req.method = toks[0];
+        
         req.headers = {};
-        for (let i of lines.slice(1)) {
-            toks = lines[i].split(":");
-            if (toks.length !== 2) {
-                throw `line ${i+1}: expects ":"`
+        let i = 1;
+        let nbreak = 0;
+        for (let header of lines.slice(1)) {
+            header = header.trim();
+            if (header === "") {
+                i++;
+                nbreak++
+                if (nbreak === 1) {
+                    break;
+                }
+                continue;
             }
-            req.headers[toks[0]] = toks[1];
+            toks = header.split(":");
+            if (toks.length !== 2) {
+                throw `line ${i}: colon are mandatory in header`;
+            }
+            req.headers[toks[0]] = toks[1].trim();
+            i++;
         }
-
-        new Request(url, req);
+        
+        console.log(req);
+        return new Request(url, req);
     }
 
     return Object.freeze(that);
